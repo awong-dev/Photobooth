cfe3a6e748444e6e6bbeaac60a19a1470be5e86b
Albert J. Wong <awong-dev@gmail.com>
2010-10-03 13:39:12 -0700

    Initial image setup correctly.

--- PhotoboothAppDelegate.h
+++ PhotoboothAppDelegate.h
@@ -2,37 +2,31 @@
 //  PhotoboothAppDelegate.h
 //  Photobooth
 //
-//  Created by Albert Wong on 7/20/10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//  Created by Albert J. Wong on 7/20/10.
+//  Copyright 2010. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
-#import <ImageCaptureCore/ImageCaptureCore.h>
+#import <Quartz/Quartz.h>
 
-@interface PhotoboothAppDelegate : NSObject <
-	NSApplicationDelegate,
-	ICDeviceBrowserDelegate,
-	ICCameraDeviceDelegate> {
-    NSWindow *window;
-		
-	// Define Interface Builder outlets for two
-	// array controllers -- one for cameras,
-	// one for the chosen camera's files
-	IBOutlet  NSArrayController *  mCamerasController;
-	IBOutlet  NSArrayController *  mMediaFilesController;
-		
-	// Define IB outlets for the tableviews used to
-	// display the cameras and the chosen camera's contents
-	IBOutlet  NSTableView * mCameraContentTableView;
-	IBOutlet  NSTableView * mCamerasTableView;
-		
-		
-	ICDeviceBrowser * mDeviceBrowser;
-	NSMutableArray * mCameras;
-
+@interface PhotoboothAppDelegate : NSObject {
+ @private
+  NSWindow *window;
+  IKImageView* mainImage;
+  IKImageBrowserView *browserView;
+  NSTextField *statusText;
+  NSURL* defaultImageUrl;
 }
 
-@property (assign) IBOutlet NSWindow *window;
-@property(retain)   NSMutableArray* cameras;
+@property(retain, nonatomic) IBOutlet NSWindow *window;
+@property(retain, nonatomic) IBOutlet IKImageView *mainImage;
+@property(retain, nonatomic) IBOutlet IKImageBrowserView *browserView;
+@property(retain, nonatomic) IBOutlet NSTextField *statusText;
+
+@property(retain, nonatomic) NSURL* defaultImageUrl;
+
+- (IBAction)takePicture:(id)pId;
+- (IBAction)scrollBrowseLeft:(id)pId;
+- (IBAction)scrollBrowseRight:(id)pId;
 
 @end
--- PhotoboothAppDelegate.m
+++ PhotoboothAppDelegate.m
@@ -2,8 +2,8 @@
 //  PhotoboothAppDelegate.m
 //  Photobooth
 //
-//  Created by Albert Wong on 7/20/10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//  Created by Albert J. Wong on 7/20/10.
+//  Copyright 2010. All rights reserved.
 //
 
 #import "PhotoboothAppDelegate.h"
@@ -12,113 +12,49 @@
 @implementation PhotoboothAppDelegate
 
 @synthesize window;
+@synthesize mainImage;
+@synthesize browserView;
+@synthesize statusText;
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-	mCameras = [[NSMutableArray alloc] initWithCapacity:0];
-	// Get an instance of ICDeviceBrowser
-    mDeviceBrowser = [[ICDeviceBrowser alloc] init];
-    // Assign a delegate
-    mDeviceBrowser.delegate = self;
-    // Look for cameras in all available locations
-    mDeviceBrowser.browsedDeviceTypeMask = mDeviceBrowser.browsedDeviceTypeMask 
-	| ICDeviceTypeMaskCamera
-	| ICDeviceLocationTypeMaskLocal
-	| ICDeviceLocationTypeMaskShared
-	| ICDeviceLocationTypeMaskBonjour
-	| ICDeviceLocationTypeMaskBluetooth
-	| ICDeviceLocationTypeMaskRemote;
-	
-	NSLog(@"Go Go device browser!");
-    // Start browsing for cameras
-    [mDeviceBrowser start];	
-}
+@synthesize defaultImageUrl;
 
-// Stop browser and release it when done
-- (void)applicationWillTerminate:(NSNotification*)notification {
-	mDeviceBrowser.delegate = NULL;
-	[mDeviceBrowser stop];
-	[mDeviceBrowser release];
-	[mCameras release];
-}
-
-// Method delegates for device added and removed
-//
-// Device browser maintains list of cameras as key-value pairs, so delegate
-// must call willChangeValueForKey to modify list
-- (void)deviceBrowser:(ICDeviceBrowser*)browser didAddDevice:(ICDevice*)addedDevice moreComing:(BOOL)moreComing
-{    
-    if ( addedDevice.type & ICDeviceTypeCamera )
-    {
-		NSEnumerator *e = [addedDevice.capabilities objectEnumerator];
-		NSString* capability;
-		NSLog(@"We want: %@", ICCameraDeviceCanTakePicture);
-		while (capability = [e nextObject]) {
-			NSLog(@"%@ has capatibilty %@", addedDevice.name, capability);
-		}
-		NSLog(@"Adding device: %@", addedDevice.name);		
-		addedDevice.delegate = self;
-		
-		// implement manual observer notification for the cameras property
-        [self willChangeValueForKey:@"cameras"];
-		[mCameras addObject:addedDevice];
-        [self didChangeValueForKey:@"cameras"];
-    }
-}
-
-
-- (void)deviceBrowser:(ICDeviceBrowser*)browser didRemoveDevice:(ICDevice*)device moreGoing:(BOOL)moreGoing
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed: (NSApplication *)sender
 {
-    device.delegate = NULL;
-    
-    // implement manual observer notification for the cameras property
-    [self willChangeValueForKey:@"cameras"];
-	[mCameras removeObject:device];
-    [self didChangeValueForKey:@"cameras"];
+  // terminate when last window was closed
+  return YES;
 }
 
-- (void)didRemoveDevice:(ICDevice*)removedDevice
+- (id)init
 {
-    [mCamerasController removeObject:removedDevice];
+  if (self = [super init])
+  {
+    NSString* defaultImagePath = @"/Users/awong/marmalade.png";
+    self.defaultImageUrl = [NSURL fileURLWithPath: defaultImagePath];
+  }
+  return self;
 }
 
-// Check to see if the camera has image files to download
-- (void)observeValueForKeyPath:(NSString*)keyPath ofObject:(id)object change:(NSDictionary*)change context:(void*)context
-{
-    if ( [keyPath isEqualToString:@"selectedObjects"] && (object == mMediaFilesController) )
-    {
-        [self willChangeValueForKey:@"canDownload"];
-        [self didChangeValueForKey:@"canDownload"];
-    }
+- (void)awakeFromNib
+{  
+  
+  [mainImage setImageWithURL:self.defaultImageUrl];
+  
+  // customize the IKImageView...
+  [mainImage setDoubleClickOpensImageEditPanel: YES];
+  [mainImage setCurrentToolMode: IKToolModeCrop];
+  [mainImage zoomImageToFit: self];
+  [mainImage setDelegate: self];
 }
 
-- (BOOL)canDownload
-{
-    if ( [[mMediaFilesController selectedObjects] count] )
-        return YES;
-    else
-        return NO;
+- (IBAction)takePicture:(id)pId {
+  NSLog(@"Smile! Taking picture.");  
 }
 
-// Download images
-// (Add a download button in interface builder -- use boolean to enable button)
-- (void)downloadFiles:(NSArray*)files
-{
-    NSDictionary* options = [NSDictionary dictionaryWithObject:[NSURL fileURLWithPath:[@"~/Pictures" stringByExpandingTildeInPath]] forKey:ICDownloadsDirectoryURL];
-    
-    for ( ICCameraFile* f in files )
-    {
-        [f.device requestDownloadFile:f options:options downloadDelegate:self didDownloadSelector:@selector(didDownloadFile:error:options:contextInfo:) contextInfo:NULL];
-    }
+- (IBAction)scrollBrowseLeft:(id)pId {
+  NSLog(@"Browse Left");
 }
 
-// Done downloading -- log results to console for debugging
-- (void)didDownloadFile:(ICCameraFile*)file error:(NSError*)error options:(NSDictionary*)options contextInfo:(void*)contextInfo
-{
-    NSLog( @"didDownloadFile called with:\n" );
-    NSLog( @"  file:        %@\n", file );
-    NSLog( @"  error:       %@\n", error );
-    NSLog( @"  options:     %@\n", options );
-    NSLog( @"  contextInfo: %p\n", contextInfo );
+- (IBAction)scrollBrowseRight:(id)pId {
+  NSLog(@"Browse Right");
 }
-
 @end
