786439801b9b84a087654e97f2947b8243478e0e
Albert J. Wong <awong-dev@gmail.com>
2010-10-09 01:21:40 -0700

    Capture, browse, update work basically.  Printing still broken. Need to get polish down.

--- PhotoboothAppDelegate.h
+++ PhotoboothAppDelegate.h
@@ -11,7 +11,14 @@
 
 #import "ImageInfo.h";
 
-@interface PhotoboothAppDelegate : NSObject {
+enum CaputreState {
+  kUninitialized,
+  kReady,
+  kTakingPicture,
+  kDownloadingFile,
+};
+
+@interface PhotoboothAppDelegate : NSObject<ICDeviceBrowserDelegate, ICCameraDeviceDelegate> {
  @private
   // UI Elements.
   NSWindow *window;
@@ -24,6 +31,11 @@
   NSString* imagesDirectory;
   ImageInfo* defaultImage;
   NSMutableArray* imageList;  // Holds images from oldest to most recent.
+  enum CaputreState state;
+  
+  // Camera search state.
+  ICDeviceBrowser* deviceBrowser;
+  NSMutableArray* cameras;
 }
 
 @property(retain, nonatomic) IBOutlet NSWindow *window;
@@ -32,7 +44,7 @@
 @property(retain, nonatomic) IBOutlet NSScrollView *browserViewContainer;
 @property(retain, nonatomic) IBOutlet NSTextField *statusText;
 
-@property(retain, nonatomic) NSString* imagesDirectory;
+@property(retain) NSString* imagesDirectory;
 @property(retain, nonatomic) ImageInfo* defaultImage;
 @property(retain, nonatomic) NSMutableArray* imageList;
 
@@ -40,14 +52,23 @@
 - (IBAction)takePicture:(id)pId;
 - (IBAction)scrollBrowseLeft:(id)pId;
 - (IBAction)scrollBrowseRight:(id)pId;
+- (IBAction)print:(id)pId;
+- (IBAction)showEffects:(id)pId;
 
 // IKImageBrowserView delegates.
 - (NSUInteger) numberOfItemsInImageBrowser:(IKImageBrowserView *)aBrowser;
 - (id) imageBrowser:(IKImageBrowserView *) aBrowser itemAtIndex:(NSUInteger)index;
 
+// ICDeviceBrowserDelegate
+- (void)deviceBrowser:(ICDeviceBrowser*)browser didAddDevice:(ICDevice*)device moreComing:(BOOL)moreComing;
+- (void)deviceBrowser:(ICDeviceBrowser*)browser didRemoveDevice:(ICDevice*)device moreGoing:(BOOL)moreGoing;
+
 // Internal functions.
 - (void)initializeMainImage;
 - (void)initializeBrowseList;
-- (void)addImagesWithPath:(NSString*) path;
-- (void)addAnImageWithPath:(NSString *)path;
+- (void)addImagesWithPath:(NSString*)path;
+- (void)addAnImageWithPath:(NSString*)path;
+- (void)setReadyText:(NSString*)deviceName;
+- (void)setDownloadingText:(NSString*)filename;
+- (void)setUninitialized;
 @end
--- PhotoboothAppDelegate.m
+++ PhotoboothAppDelegate.m
@@ -9,6 +9,7 @@
 #import "PhotoboothAppDelegate.h"
 
 #import <Quartz/Quartz.h>
+#import <ImageCaptureCore/ImageCaptureCore.h>
 
 @implementation PhotoboothAppDelegate
 
@@ -36,7 +37,8 @@
           [NSHomeDirectory() stringByAppendingPathComponent:@"marmalade.png"]];
     imageList = [[NSMutableArray alloc] init];
     imagesDirectory =
-      [NSHomeDirectory() stringByAppendingPathComponent:@"test"];
+      [[NSHomeDirectory() stringByAppendingPathComponent:@"test"] retain];
+    state = kUninitialized;
   }
   return self;
 }
@@ -44,13 +46,46 @@
 - (void)awakeFromNib {
   [self initializeMainImage];
   [self initializeBrowseList];
+  [self setUninitialized];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification*)notification
+{
+  cameras = [[NSMutableArray alloc] initWithCapacity:0];
+  
+  // Get an instance of ICDeviceBrowser
+  deviceBrowser = [[ICDeviceBrowser alloc] init];
+  // Assign a delegate
+  deviceBrowser.delegate = self;
+  
+  // We only want local caneras.
+  deviceBrowser.browsedDeviceTypeMask = deviceBrowser.browsedDeviceTypeMask 
+    | ICDeviceTypeMaskCamera
+    | ICDeviceLocationTypeMaskLocal;
+
+  [deviceBrowser start];  
+}
+
+- (void)applicationWillTerminate:(NSNotification*)notification {
+  deviceBrowser.delegate = NULL;
+  [deviceBrowser stop];
+  [deviceBrowser release];
+  [cameras release];
 }
 
 //
 // ------ UI Actions -------
 //
 - (IBAction)takePicture:(id)pId {
-  NSLog(@"Smile! Taking picture.");  
+  if (state != kReady) {
+    // TODO(awong): Pop up an eror?
+    return;
+  }
+  
+  [[cameras lastObject] requestTakePicture];
+  state = kTakingPicture;
+  // TODO(ajwong): Setup watchdog timer that will close session and restart if
+  // download does not complete in 3 seconds.
 }
 
 - (IBAction)scrollBrowseLeft:(id)pId {
@@ -73,6 +108,25 @@
   [scrollContents scrollToPoint: newOrigin];
   [browserViewContainer reflectScrolledClipView: scrollContents];
 }
+
+- (IBAction)print:(id)pId {
+  // If the image has edits, save to disk.
+  // Print the main image.
+  NSPrintInfo* origInfo = [NSPrintInfo sharedPrintInfo];
+  NSPrintInfo* newInfo = [[NSPrintInfo alloc] initWithDictionary:[origInfo dictionary]];
+  [newInfo setVerticalPagination:NSFitPagination];
+  [newInfo setHorizontalPagination:NSFitPagination];
+  
+  NSPrintOperation *op =
+     [NSPrintOperation printOperationWithView:mainImage printInfo:newInfo];
+  [op setShowsPrintPanel:NO];
+  [op setCanSpawnSeparateThread:YES];
+  [op runOperation];
+}
+
+- (IBAction)showEffects:(id)pId {
+}
+
 
 //
 // ------ IKImageBrowserView delegates------
@@ -102,8 +156,8 @@
   [mainImage setImageWithURL:self.defaultImage.url];
   
   // customize the IKImageView...
-  [mainImage setDoubleClickOpensImageEditPanel: YES];
-  [mainImage setCurrentToolMode: IKToolModeCrop];
+  [mainImage setDoubleClickOpensImageEditPanel: NO];
+  [mainImage setCurrentToolMode: IKToolModeNone];
   [mainImage zoomImageToFit: self]; 
   [mainImage setDelegate: self];
 }
@@ -124,15 +178,15 @@
   NSString *file;
   while (file = [dirEnum nextObject]) {
     NSString* extension = [file pathExtension];
-    if ([extension isEqualToString: @"jpg"] ||
-        [extension isEqualToString: @"png"]) {
+    if ([extension caseInsensitiveCompare:@"jpg"] == NSOrderedSame ||
+        [extension caseInsensitiveCompare:@"png"] == NSOrderedSame) {
       [self addAnImageWithPath:[imagesDirectory stringByAppendingPathComponent:file]];
     }
   }
   [localFileManager release];
 }
 
-- (void) addAnImageWithPath:(NSString *) path
+- (void)addAnImageWithPath:(NSString *) path
 {
   ImageInfo *imageInfo;
 
@@ -140,5 +194,137 @@
   [imageList addObject:imageInfo];
   [imageInfo release];
 }
+
+
+// ----- ICDeviceBrowser protocol -----
+
+// Method delegates for device added and removed
+//
+// Device browser maintains list of cameras as key-value pairs, so delegate
+// must call willChangeValueForKey to modify list
+- (void)deviceBrowser:(ICDeviceBrowser*)browser didAddDevice:(ICDevice*)addedDevice moreComing:(BOOL)moreComing
+{    
+  if ( addedDevice.type & ICDeviceTypeCamera )
+  {
+    if ([addedDevice.capabilities indexOfObject:ICCameraDeviceCanTakePicture] == NSNotFound) {
+      NSLog(@"Device: %@ cannot take pictures", addedDevice.name);		
+      return;
+    }
+    NSLog(@"Adding device: %@", addedDevice.name);
+    addedDevice.delegate = self;
+    
+    // implement manual observer notification for the cameras property
+    [self willChangeValueForKey:@"cameras"];
+    [cameras addObject:addedDevice];
+    [self didChangeValueForKey:@"cameras"];
+
+    [[cameras lastObject] requestOpenSession];
+  }
+}
+
+
+- (void)deviceBrowser:(ICDeviceBrowser*)browser didRemoveDevice:(ICDevice*)device moreGoing:(BOOL)moreGoing
+{
+  device.delegate = NULL;
+  
+  // implement manual observer notification for the cameras property
+  [self willChangeValueForKey:@"cameras"];
+  [cameras removeObject:device];
+  [self didChangeValueForKey:@"cameras"];
+
+  // Open the new camera if there is one.
+  if ([cameras count] == 0) {
+    [self setUninitialized];
+  } else {
+    [[cameras lastObject] requestOpenSession];
+  }
+}
+
+// Check to see if the camera has image files to download
+- (void)observeValueForKeyPath:(NSString*)keyPath ofObject:(id)object change:(NSDictionary*)change context:(void*)context
+{
+  if ( [keyPath isEqualToString:@"selectedObjects"] && (object == self) )
+  {
+    [self willChangeValueForKey:@"canDownload"];
+    [self didChangeValueForKey:@"canDownload"];
+  }
+}
+
+// ---- ICCameraDeviceDelegate ----
+
+- (void)didRemoveDevice:(ICDevice*)device {
+  // Do nothing. device browser delegate handles our state.
+}
+
+- (void)didDownloadFile:(ICCameraFile*)file
+                  error:(NSError*)error
+                options:(NSDictionary*)options
+            contextInfo:(void*)contextInfo {
+  // TODO(ajwong): Add newly found file into browser.
+  NSString* filename = [options objectForKey:ICSavedFilename];
+  [self addAnImageWithPath:[imagesDirectory stringByAppendingPathComponent:filename]];
+  [browserView reloadData];
+  [self setReadyText: [[file device] name]];
+}
+
+- (void)cameraDevice:(ICCameraDevice*)camera didAddItem:(ICCameraItem*)item {
+  NSLog(@"Item name %@", [item name]);
+  if (state != kTakingPicture) {
+    // Ignore random item adds since we only want the result of the snapshot.
+    // The camera seems to like to send all its pictures at startup, so this
+    // is useful.
+    return;
+  }
+  if (![item isKindOfClass:[ICCameraFile class]]) {
+    // We only handle files.
+    return;
+  }
+  
+  // Download it if it's an image.
+  if ([[item UTI] isEqualToString:(NSString*)kUTTypeImage]) {
+    NSDictionary* downloadOptions = [NSDictionary
+      dictionaryWithObject:[NSURL fileURLWithPath:imagesDirectory]
+                    forKey:ICDownloadsDirectoryURL];
+
+    [camera requestDownloadFile:(ICCameraFile*)item
+                        options:downloadOptions
+           downloadDelegate:self
+            didDownloadSelector:@selector(didDownloadFile:error:options:contextInfo:)
+                contextInfo:NULL];
+    [self setDownloadingText:[item name]];
+  }
+}
+
+- (void)device:(ICDevice*)device didOpenSessionWithError:(NSError*)error {
+  if (error != nil) {
+    NSLog(@"Error %@", error);
+    return;
+  }
+  [self setReadyText:[device name]];
+}
+
+- (void)device:(ICDevice*)device didCloseSessionWithError:(NSError*)error {
+  if (error != nil) {
+    NSLog(@"Error %@", error);
+  }
+}
+
+- (void)setReadyText:(NSString*)deviceName {
+  state = kReady;
+  [statusText setStringValue:
+      [[NSString alloc] initWithFormat:@"%@ Ready", deviceName]];  
+}
+
+- (void)setDownloadingText:(NSString*)filename {
+  state = kDownloadingFile;
+  [statusText setStringValue:
+      [[NSString alloc] initWithFormat:@"Downloading %@", filename]];  
+}
+
+- (void)setUninitialized {
+  state = kUninitialized;
+  [statusText setStringValue:@"No Camera Found"];
+}
+
 
 @end
